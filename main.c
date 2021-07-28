5161993633192dcde9ff26bd5c87b6f97c60f917
ehelmine <ella.helminen@outlook.com>
2021-07-28 17:54:21 +0300

    added notes, char **environ is important..

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ehelmine <ehelmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:14:45 by ehelmine          #+#    #+#             */
-/*   Updated: 2021/07/28 17:13:51 by ehelmine         ###   ########.fr       */
+/*   Updated: 2021/07/28 17:53:27 by ehelmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,28 @@
 // killall 21sh
 
 // CHECK WHAT FORK RETURNS..
+// • You must program a mini UNIX command interpreter.
+// • This interpreter must display a prompt (a simple "$> " for example) and wait till
+// you type a command line, validated by pressing enter.
+// • The prompt is shown again only once the command has been completely executed.
+// • The command lines are simple, no pipes, no redirections or any other advanced
+// functions.
+// • The executable are those you can find in the paths indicated in the PATH variable.
+// • In cases where the executable cannot be found, it has to show an error message and
+// display the prompt again.
+// • You must manage the errors without using errno, by displaying a message adapted
+// to the error output.
+// • You must deal correctly with the PATH and the environment (copy of system char
+// **environ).
+// • You must implement a series of builtins: echo, cd, setenv, unsetenv, env, exit.
+// • You can choose as a reference whatever shell you prefer.
+// • You must manage expansions $ and ˜
 
 int main() 
 {
 	char buf[100];
 	char *argv[] = {"ls", NULL, NULL};
-  char *envp[] = {NULL };
+ 	char *my_env_args[] = { "PATH=/bin", "USER=me", NULL };
 	char c;
 	int x;
 	int i;
@@ -60,7 +76,7 @@ int main()
 				argv[1] = arr[1];
 			if(child_pid == 0)
 			{
-			  if (execve("/bin/ls", argv, envp) == -1)
+			  if (execve("/bin/ls", argv, my_env_args) == -1)
 			  	perror("childpid 0");
 			}
 			else if (child_pid < 0)
