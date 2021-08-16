3aecacd9e545fb6eaf251dbcebe180a669654a71
ehelmine <ella.helminen@outlook.com>
2021-08-16 16:47:26 +0300

    echo with $variable works

--- built_in.c
+++ built_in.c
@@ -6,7 +6,7 @@
 /*   By: ehelmine <ehelmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:57:03 by ehelmine          #+#    #+#             */
-/*   Updated: 2021/08/16 12:57:17 by ehelmine         ###   ########.fr       */
+/*   Updated: 2021/08/16 16:45:03 by ehelmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,52 @@
 **
 */
 
-void	my_echo(char *input)
+void	change_to_uppercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_islower(str[i]))
+			str[i] -= 32;
+		i++;
+	}
+}
+
+char	*print_dollar(char **environ, char *echo_arg)
+{
+	int i;
+	int len;
+	char *temp;
+
+	i = 0;
+	echo_arg++;
+	while (echo_arg[i] != ' ' && echo_arg[i] != '\0')
+		i++;
+	temp = ft_strndup(echo_arg, i);
+	change_to_uppercase(temp);
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		if (ft_strstr(environ[i], temp))
+		{
+			len = ft_strlen(temp);
+			free(temp);
+			ft_putstr(environ[i]);
+			return (echo_arg + len);
+		}
+		else
+			i++;
+	}
+	return (NULL);	
+}
+
+void	my_echo(char *input, char **copy_of_environ)
 {
 	int i;
 	char *echo_arg;
+	int len;
 	
 	i = 0;
 	while (input[i] == ' ')
@@ -29,8 +71,18 @@ void	my_echo(char *input)
 	if (echo_arg != NULL)
 	{
 		echo_arg++;
+		if (echo_arg[0] == '$')
+			echo_arg = print_dollar(copy_of_environ, echo_arg);
 		if (echo_arg[0] != '\0' && (echo_arg[0] != ';' || echo_arg[0] != '|'))
-			ft_putstr(echo_arg);
+		{
+			if (echo_arg[0] == '"')
+				echo_arg++;
+			len = ft_strlen(echo_arg);
+			if (echo_arg[len - 1] == '"')
+				write(1, echo_arg, len - 1);
+			else
+				write(1, echo_arg, len);
+		}
 	}
 	write(1, "\n", 1);
 	return ;	
@@ -46,7 +98,7 @@ void	execute_built_in(char *built_in, char **copy_of_environ, char **args)
 	else if (built_in[0] == 'c')
 		ft_printf("--im cd |%s|\n", built_in);
 	else if (built_in[0] == 'e' && built_in[1] == 'c')
-		my_echo(args[0]);
+		my_echo(args[0], copy_of_environ);
 	else if (built_in[0] == 's')
 		ft_printf("--im setenv |%s|\n", built_in);
 	else if (built_in[0] == 'u')
