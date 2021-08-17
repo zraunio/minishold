2f5c3296faeb67577b89553d0846e73ed8cfe60b
ehelmine <ella.helminen@outlook.com>
2021-08-17 14:48:43 +0300

    echo works

--- built_in.c
+++ built_in.c
@@ -6,7 +6,7 @@
 /*   By: ehelmine <ehelmine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:57:03 by ehelmine          #+#    #+#             */
-/*   Updated: 2021/08/16 16:45:03 by ehelmine         ###   ########.fr       */
+/*   Updated: 2021/08/17 14:48:06 by ehelmine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,54 +34,154 @@ char	*print_dollar(char **environ, char *echo_arg)
 	int i;
 	int len;
 	char *temp;
+	int x;
+	char *check;
 
 	i = 0;
 	echo_arg++;
-	while (echo_arg[i] != ' ' && echo_arg[i] != '\0')
+	if (echo_arg[i] == ' ')
+		return (echo_arg);
+	while (echo_arg[i] != ' ' && echo_arg[i] != '"' && echo_arg[i] != '\0')
 		i++;
+	temp = NULL;
 	temp = ft_strndup(echo_arg, i);
 	change_to_uppercase(temp);
 	i = 0;
+	len = ft_strlen(temp);
 	while (environ[i] != NULL)
 	{
-		if (ft_strstr(environ[i], temp))
+		check = ft_strstr(environ[i], temp);
+		if (check != NULL)
 		{
-			len = ft_strlen(temp);
+			x = 0;
+			if (ft_strcmp(temp, "PATH") == 0)
+			{
+				free(temp);
+				temp = ft_strdup(environ[i] + (ft_strlen(temp) + 1));
+				while (temp[x] != '\0')
+				{
+					if (temp[x] == ':')
+						temp[x] = ' ';
+					x++;
+				}
+				ft_putstr(temp);
+			}
+			else
+				ft_putstr(environ[i] + ft_strlen(temp) + 1);
+			if (echo_arg + len != NULL)
+				write(1, " ", 1);
 			free(temp);
-			ft_putstr(environ[i]);
+			temp = NULL;
 			return (echo_arg + len);
 		}
 		else
 			i++;
 	}
-	return (NULL);	
+	free(temp);
+	return (echo_arg + len);	
+}
+
+char	*print_double_quotes(char *echo_arg, char **copy_of_environ)
+{
+	int i;
+	char *print_me;
+	int x;
+	char *tmp;
+
+	i = 1;
+	while (echo_arg[i] != '\0')
+	{
+		if (echo_arg[i] == '$')
+		{
+			if (i > 1)
+				write(1, echo_arg + 1, i - 1);
+			tmp = ft_strchr(echo_arg, '$');
+			x = 0;
+			while (tmp[x] != '\0')
+			{
+				if (tmp[x] == ' ')
+					break ;
+				x++;
+			}
+			print_me = ft_strndup(tmp, x);
+			echo_arg = print_dollar(copy_of_environ, print_me);
+			echo_arg = tmp + (ft_strlen(print_me));
+			free(print_me);
+			if (echo_arg[0] == '\0')
+				return (echo_arg);
+			i = -1;
+		}
+		else if (echo_arg[i] == '"')
+			break ;
+		i++;
+	}
+	if (i == 0)
+		return (echo_arg + i);
+	if (i == 2)
+	{
+		write(1, " ", 1);
+		return (echo_arg + i);
+	}
+	print_me = ft_strndup(echo_arg + 1, i - 1);
+	ft_putstr(print_me);
+	free(print_me);
+	if (echo_arg + i != NULL)
+		write(1, "  ", 1);
+	return (echo_arg + i);
+}
+
+char	*print_text(char *echo_arg)
+{
+	int i;
+
+	i = 0;
+	while (echo_arg[i] != '\0')
+	{
+		if (echo_arg[i] == '"' || echo_arg[i] == '$')
+			break ;
+		i++;
+	}
+	if (i < (int)ft_strlen(echo_arg))
+		write(1, echo_arg, i);
+	else
+		ft_putstr(echo_arg);
+	return (echo_arg + i);
 }
 
 void	my_echo(char *input, char **copy_of_environ)
 {
 	int i;
 	char *echo_arg;
-	int len;
+	char *tmp;
 	
 	i = 0;
+	tmp = NULL;
 	while (input[i] == ' ')
 		i++;
 	input += i;
 	echo_arg = ft_strstr(input, " ");
 	if (echo_arg != NULL)
 	{
-		echo_arg++;
-		if (echo_arg[0] == '$')
-			echo_arg = print_dollar(copy_of_environ, echo_arg);
-		if (echo_arg[0] != '\0' && (echo_arg[0] != ';' || echo_arg[0] != '|'))
+		i = 0;
+		while (echo_arg[i] != '\0')
 		{
-			if (echo_arg[0] == '"')
-				echo_arg++;
-			len = ft_strlen(echo_arg);
-			if (echo_arg[len - 1] == '"')
-				write(1, echo_arg, len - 1);
-			else
-				write(1, echo_arg, len);
+			while (echo_arg[i] == ' ')
+				i++;
+			if (echo_arg[i] == '$')
+			{
+				echo_arg = print_dollar(copy_of_environ, echo_arg + i);
+				i = 0;
+			}
+			else if (echo_arg[i] == '"')
+			{
+				echo_arg = print_double_quotes(echo_arg + i, copy_of_environ);
+				i = 0;
+			}
+			else if (echo_arg[i] != '|' || echo_arg[i] != ';')
+			{
+				echo_arg = print_text(echo_arg + i);
+				i = 0;
+			}
 		}
 	}
 	write(1, "\n", 1);
