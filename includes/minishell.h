d9a20cc951c5c7ac520c92e1d81b847e8ea31648
Rori Ivanovo <zraunio@student.hive.fi>
2021-08-17 10:14:34 +0300

    struct shell for data storage in .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehelmine <ehelmine@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 16:17:31 by ehelmine          #+#    #+#             */
-/*   Updated: 2021/08/14 15:57:56 by ehelmine         ###   ########.fr       */
+/*   Updated: 2021/08/17 10:14:01 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,25 @@
 # include <sys/stat.h>
 # include <stdio.h>
 
+// typedef struct s_shell
+// {
+// 	char	**env;
+// 	char	**tok;
+// 	char	**hist;
+// 	char	*tok;
+// 	t_lexer	*lex;
+// 	size_t	n_hist;
+// 	char	*hist_path;
+// 	int		hist_fd;
+// 	char	**hash_str;
+// 	char	*hash_one[H_SIZE];
+// 	char	buff[MAX_BUFF];
+// 	char	**args;
+// 	char	**sh_args;
+// 	int		n;
+// 	int		logo;
+// }				t_shell
+
 char	**get_copy_of_environment_variables();
 
 char	**get_paths_to_array(char **environ);
@@ -28,5 +47,6 @@ void	fork_and_child(char **path_array, char **copy_of_environ);
 char	**split_input_to_array(char *buf);
 
 void	execute_built_in(char *built_in, char **copy_of_environ, char **args);
+
 
 #endif
