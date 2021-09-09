3a304159df6fdf603818df2b4a1656bb5475e859
Rori Ivanovo <zraunio@student.hive.fi>
2021-09-09 10:50:30 +0300

    more

--- builtin/echo/echo_write.c
+++ builtin/echo/echo_write.c
@@ -5,35 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zraunio <zraunio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/02 16:16:54 by zraunio           #+#    #+#             */
-/*   Updated: 2021/09/09 10:42:53 by zraunio          ###   ########.fr       */
+/*   Created: 2021/09/09 10:48:16 by zraunio           #+#    #+#             */
+/*   Updated: 2021/09/09 10:48:28 by zraunio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/minishell.h"
+#include "minishell.h"
 
-char	*echo_write(char *out, size_t len, size_t in_or_out, size_t n)
+int	handle_newline(char *out, int i)
 {
-	size_t i;
+	if (out[i + 1] == 'n' || out[i + 2] == 'n')
+		write(1, "\n", 1);
+	else if (out[i + 1] == 't' || out[i + 2] == 't')
+		write(1, "\t", 1);
+	if (out[i + 1] == '\\' && (out[i + 2] == 'n'
+			|| out[i + 2] == 't'))
+		i++;
+	i += 2;
+	return (i);
+}
+
+int	handle_slash(char *out, int i, int in_or_out)
+{
+	if (in_or_out == 2 && (out[i + 1] == 'n'
+			|| (out[i + 1] == '\\' && out[i + 2] == 'n')
+			|| out[i + 1] == 't' || (out[i + 1] == '\\'
+				&& out[i + 2] == 't')))
+		return (handle_newline(out, i));
+	else if (in_or_out == 0)
+	{
+		if (out[i + 1] == '\\' && (out[i + 2] == 'n'
+				|| out[i + 2] == 't'))
+		{
+			if (out[i + 2] == 'n')
+				write(1, "\n", 1);
+			else if (out[i + 2] == 't')
+				write(1, "\t", 1);
+			i += 3;
+		}
+	}
+	else if (in_or_out == 2)
+	{
+		i++;
+		if (out[i + 1] == ' ')
+			write(1, &out[i++], 1);
+	}
+	return (i);
+}
+
+void	echo_write(char *out, int output_len, int in_or_out,
+	int quote)
+{
+	int	i;
 
 	i = 0;
-	while (out[i] && i < len)
+	while (out[i] != '\0' && i < output_len)
 	{
 		while (out[i] == '\\')
-		{
-			if (in_or_out == 2 && (out[i + 1] == 'n'
-			|| (out[i + 1] == '\\' && out[i + 2] == 'n') || out[i + 1] == 't' 
-			|| (out[i + 1] == '\\' && out[i + 2] == 't')))
-			{
-				if (out[i + 1] == 'n' || out[i + 2] == 'n')
-					write(1, "\n", 1);
-				else if (out[i + 1] == 't' || out[i + 2] == 't')
-					write(1, "\t", 1);
-				if (out[i + 1] == '\\' && (out[i + 2] == 'n'
-					|| out[i + 2] == 't'))
-					i++;
-				i += 2;
-			}
-		}
+			i = handle_slash(out, i, in_or_out);
+		if ((out[i] == '"' && (quote == 2 || quote == 0))
+			|| (out[i] == '\'' && (quote == 1 || quote == 0))
+			|| out[i] == '\0')
+			break ;
+		write(1, &out[i++], 1);
 	}
 }
